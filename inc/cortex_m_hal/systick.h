9ad9962f9e3bd9bc94a3bac879ea8a837ec8b5b1
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-07-30 00:18:14 +0200

    Add listener to systick

--- inc/cortex_m_hal/systick.h
+++ inc/cortex_m_hal/systick.h
@@ -8,6 +8,12 @@ extern "C" void SysTick_Handler();
 
 class SysTick {
  public:
+  class Listener {
+   public:
+    virtual void tickTock(std::uint32_t ticks) = 0;
+    virtual ~Listener() = default;
+  };
+
   constexpr static uint32_t TICKS_PER_SECOND = 1000;
 
   void init(uint32_t core_clk_mhz);
@@ -17,6 +23,8 @@ class SysTick {
   uint32_t getCoarseTickCount() { return m_ticks; }
   void delay(uint32_t coarse_ticks);
 
+  void set_listener(Listener* listener) { m_listener = listener; }
+
   static SysTick& getInstance() {
     static SysTick systick;
     return systick;
@@ -25,8 +33,12 @@ class SysTick {
  private:
   volatile uint32_t m_ticks{0};
   uint32_t m_max_count = 0;
+  Listener* m_listener = nullptr;
 
   SysTick() = default;
+
+  void handleIrq();
+
   friend void ::SysTick_Handler();
 };
 
